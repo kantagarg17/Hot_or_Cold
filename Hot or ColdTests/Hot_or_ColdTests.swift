6c4fadc700ca01cb9f71f5c6be892c735b95a32d
kantagarg17 <53440019+kantagarg17@users.noreply.github.com>
2021-03-04 16:34:20 +0530

    Add files via upload

---
+++ Hot or ColdTests/Hot_or_ColdTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hot_or_ColdTests.swift
+//  Hot or ColdTests
+//
+//  Created by Rajeev Garg on 04/06/20.
+//  Copyright © 2020 Rajeev Garg. All rights reserved.
+//
+
+import XCTest
+@testable import Hot_or_Cold
+
+class Hot_or_ColdTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
